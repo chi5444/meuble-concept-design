083cb296f37a8260927870da199b10b5bd2fa8f2
chi5444 <101504281+chi5444@users.noreply.github.com>
2026-03-27 20:44:24 +0100

    Add files via upload

--- js/supabase.js
+++ js/supabase.js
@@ -4,6 +4,6 @@ const SUPABASE_URL = 'https://jzvwfjwskejsiuypdylz.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6dndmandza2Vqc2l1eXBkeWx6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ2MTQxNzgsImV4cCI6MjA5MDE5MDE3OH0.InmYVnxf8q9aw5OnfwTufUTQtsdHpB2J2MD6a2hVw2o';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-export const ADMIN_EMAIL_DOMAIN = 'meubleconcept.tn';
-export const ADMIN_EMAIL = 'admin@meubleconcept.tn';
+export const ADMIN_EMAIL_DOMAIN = 'meubleconcept.com';
+export const ADMIN_EMAIL = 'admin@meubleconcept.com';
 export const STORAGE_BUCKET = 'product-images';
